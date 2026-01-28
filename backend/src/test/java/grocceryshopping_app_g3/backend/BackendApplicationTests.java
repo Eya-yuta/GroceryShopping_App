caae4740570c53afc93b6e1b2a89cef2d60b2e38
Eya <benhasseneya28@gmail.com>
2026-01-28 10:01:56 +0100

    update gitignore and add backend module (2)

---
+++ backend/src/test/java/grocceryshopping_app_g3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package grocceryshopping_app_g3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
